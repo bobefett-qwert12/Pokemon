c63361c86b9947634707d0c7044281c043998a20
RyanPerkins <bobefett.qwert12@gmail.com>
2019-02-04 09:34:32 -0700

    filled out the panel with text fields and labels for the pokemon's attributes

--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -15,12 +15,33 @@ public class PokedexPanel extends JPanel
 	private JTextField attackPointsText;
 	private JTextField enhancementModifierText;
 	private JTextField numberText;
+	private JTextField nameText;
+	private JTextField canEvolveText;
+	private JLabel healthPointsLabel;
+	private JLabel attackPointsLabel;
+	private JLabel enhancementModifierLabel;
+	private JLabel numberLabel;
+	private JLabel nameLabel;
+	private JLabel canEvolveLabel;
 	
 	public PokedexPanel(PokedexController appController)
 	{
 		super();
 		this.appController = appController;
 		appLayout = new SpringLayout();
+		
+		healthPointsText = new JTextField("");
+		attackPointsText = new JTextField("");
+		enhancementModifierText = new JTextField("");
+		numberText = new JTextField("");
+		nameText = new JTextField("");
+		canEvolveText = new JTextField("");
+		healthPointsLabel = new JLabel("Health Points:");
+		attackPointsLabel = new JLabel("Attack Points:");
+		enhancementModifierLabel = new JLabel("Enhancement Modifier:");
+		numberLabel = new JLabel("Pokemon Number:");
+		nameLabel = new JLabel("Pokemon Name:");
+		canEvolveLabel = new JLabel("Can Evolve:");
 		
 		setupPanel();
 		setupLayout();
@@ -32,7 +53,18 @@ public class PokedexPanel extends JPanel
 		this.setLayout(appLayout);
 		this.setPreferredSize(new Dimension(800, 600));
 		this.setBackground(Color.darkGray);
-		//this.add();
+		this.add(healthPointsText);
+		this.add(attackPointsText);
+		this.add(enhancementModifierText);
+		this.add(numberText);
+		this.add(nameText);
+		this.add(canEvolveText);
+		this.add(healthPointsLabel);
+		this.add(attackPointsLabel);
+		this.add(enhancementModifierLabel);
+		this.add(numberLabel);
+		this.add(nameLabel);
+		this.add(canEvolveLabel);
 	}
 	
 	private void setupLayout()
